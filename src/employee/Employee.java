522fa65f0c78ac9927c56891097e4ab9ae42d837
Colin <ciciccz@gmail.com>
2022-09-25 14:09:52 -0600

    Refined methods, completed class implementation and testing.

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -8,7 +8,7 @@ package employee;
  */
 public class Employee {
 
-	// Constant used to process weekly gross pay, deductions, and net pay
+	// Constants used to process weekly gross pay, deductions, and net pay
 	public static final int NUM_YEARLY_WEEKS = 52;
 	public static final int WEEKLY_HOURS = 40;
 	public static final int WEEKLY_MAX_HOURS = 60;
@@ -44,7 +44,8 @@ public class Employee {
 		this.type = type;
 		this.payRate = payRate;
 		this.maxHours = maxHours;
-	}
+		}
+	
 
 	/**
 	 * Copy parameterized constructor for Employee class
@@ -231,116 +232,101 @@ public class Employee {
 	 * Method to calculate employee's federal income tax deductions
 	 * 
 	 * @param grossWkPay double with employee's gross pay per week
-	 * @return double with employee's deducted amount from Federal Income Tax
+	 * @return double with employee's deducted amount from Federal Income Tax; applicable to all employees
 	 * @author Colin Cui and rafaelalarcon
 	 */
 	public double calcWithhold(double grossWkPay) {
 
-		final double FIRST_TAX_BRACKET = 1000 * 0.075;
-		final double SECOND_TAX_BRACKET = 1000 * 0.12;
-
-		if (grossWkPay < 1000) {
-			return grossWkPay * 0.075;
-		} else if (grossWkPay < 2000) {
-			return (grossWkPay - 1000) * 0.12 + FIRST_TAX_BRACKET;
-		}
-		return (grossWkPay - 2000) * 0.17 + FIRST_TAX_BRACKET + SECOND_TAX_BRACKET;
-
+		final double FIRST_BRACKET_AMOUNT = 1000;
+		final double SECOND_BRACKET_AMOUNT = 2000;
+		final double FIRST_TAX_RATE = 0.075;
+		final double SECOND_TAX_RATE = 0.12;
+		final double THIRD_TAX_RATE = 0.17;
+		final double FIRST_BRACKET_TOTAL = FIRST_BRACKET_AMOUNT * FIRST_TAX_RATE;
+		final double SECOND_BRACKET_TOTAL = FIRST_BRACKET_AMOUNT * SECOND_TAX_RATE;
+		
+		return grossWkPay < FIRST_BRACKET_AMOUNT ? grossWkPay * FIRST_TAX_RATE
+				:grossWkPay > SECOND_BRACKET_AMOUNT ? (grossWkPay - SECOND_BRACKET_AMOUNT) 
+						* THIRD_TAX_RATE + FIRST_BRACKET_TOTAL + SECOND_BRACKET_TOTAL
+				:(grossWkPay - FIRST_BRACKET_AMOUNT) * SECOND_TAX_RATE + FIRST_BRACKET_TOTAL;
 	}
 
 	/**
 	 * Method to calculate employee's CPP deduction
 	 * 
 	 * @param grossWkPay double with employee's gross pay per week
-	 * @return double with employees deducted amount from CPP
+	 * @return double with employees deducted amount from CPP; applicable to all employees
 	 * @author Colin Cui and rafaelalarcon
 	 */
 	public double calcCPP(double grossWkPay) {
-		return grossWkPay * 0.0475;
+		final double CPP_RATE = 0.0475;
+		return grossWkPay * CPP_RATE;
 	}
 
 	/**
 	 * Method to calculate employee's EI deduction
 	 * 
 	 * @param grossWkPay double with employees gross pay per week
-	 * @return double with employee's deducted amount from EI
+	 * @return double with employee's deducted amount from EI; applicable to all employees
 	 * @author Colin Cui and rafaelalarcon
 	 */
 	public double calcEI(double grossWkPay) {
-		return grossWkPay * 0.018;
+		final double EI_RATE = 0.018;
+		return grossWkPay * EI_RATE;
 	}
 
 	/**
 	 * Method to calculate employees' extended health benefit deduction
 	 * 
 	 * @param grossWkPay double with employees gross pay per week
-	 * @return double with employee's deducted amount from extended health benefit
+	 * @return double with employee's deducted amount from extended health benefit; exludes contractor type
 	 * @author Colin Cui and rafaelalarcon
 	 */
 	public double calcExtHealth(double grossWkPay) {
-		if (type == 'S' || type == 'H') {
-			return grossWkPay * 0.013;
-		}
-		return 0;
+		final double EXT_HEALTH_RATE = 0.013;
+		return type == EMPLOYEE_TYPE_CONTRACTOR ? 0 : grossWkPay * EXT_HEALTH_RATE;
 	}
 
 	/**
 	 * Method to calculate employee's union dues deduction
 	 * 
 	 * @param grossWkPay double with employees gross pay per week
-	 * @return double with employee's deducted amount from union dues
+	 * @return double with employee's deducted amount from union dues; only Hourly
 	 * @author Colin Cui and rafaelalarcon
 	 */
 	public double calcUnionDues(double grossWkPay) {
-		if (type == 'H') {
-			return grossWkPay * 0.01;
-		}
-		return 0;
+		final double UNION_RATE = 0.01;
+		return type == EMPLOYEE_TYPE_HOURLY ? grossWkPay * UNION_RATE : 0;
+	}
+	
+	/**
+	 * 
+	 * @param grossWkPay
+	 * @return double; total of all deductions of an employee.
+	 */
+	public double calcAllDeductions(double grossWkPay) {
+		return calcWithhold(grossWkPay) + calcCPP(grossWkPay) + calcEI(grossWkPay) + 
+				calcExtHealth(grossWkPay) + calcUnionDues(grossWkPay);
 	}
 
 	/**
 	 * 
-	 * @param grossPay
-	 * @return
+	 * @param double, hoursWorked weekly
+	 * @return double, net pay weekly 
 	 */
-	public double calcNetPay(double grossPay) {
-		switch (type) {
-		case EMPLOYEE_TYPE_SALARY:
-			double salaryTaxDeductedPay = commonDeductions(grossPay) - calcExtHealth(grossPay);
-			return salaryTaxDeductedPay;
-		case EMPLOYEE_TYPE_HOURLY:
-			double hourlyTaxDeductedPay = commonDeductions(grossPay) - calcExtHealth(grossPay)
-					- calcUnionDues(grossPay);
-			return hourlyTaxDeductedPay;
-		case EMPLOYEE_TYPE_CONTRACTOR:
-			double contractorTaxDeductedPay = commonDeductions(grossPay);
-			return contractorTaxDeductedPay;
-		default:
-			return 4;
-		}
-
+	public double calcNetPay(double hoursWorked) {
+		double grossPay = calcGrossPay(hoursWorked);
+		return grossPay - calcAllDeductions(grossPay);
 	}
-
 	/**
 	 * 
-	 * @param obj
-	 * @return
+	 * @param otherEmployee
+	 * @return int; 1 if this employeeNo is higher than the one compared. 0 if same. -1 if lower.
 	 */
-	public int compareTo(Object obj) {
-		if (this == obj)
-			return 0;
-		if (obj == null)
-			return -1;
-		if (getClass() != obj.getClass())
-			return -1;
-		Employee other = (Employee) obj;
-		if (this.getEmpNo() < other.getEmpNo()) {
-			return -1;
-		}
-		if (this.getEmpNo() > other.getEmpNo()) {
-			return 1;
-		}
-		return 0;
+	public int compareTo(Employee otherEmployee) {
+		return empNo > otherEmployee.getEmpNo() ? 1 
+				:empNo < otherEmployee.getEmpNo() ? -1 
+				:0;
 	}
 
 	/**
@@ -354,18 +340,10 @@ public class Employee {
 				getEmpNo(), getEmpName(), getDeparment(), getTypeFull(), getPayRate(), getMaxHours());
 
 	}
-
 	/**
-	 * Helper method to calculate deductions common to all employees type
-	 * 
-	 * @param grossPay double with employee's calculated gross pay
-	 * @return double with employee's calculated common deductions
-	 * @author Colin Cui and rafaelalarcon
+	 * Helps toString() to display full name of the employee type. 
+	 * @return String, type of employee
 	 */
-	private double commonDeductions(double grossPay) {
-		return grossPay - calcWithhold(grossPay) - calcCPP(grossPay) - calcEI(grossPay);
-	}
-
 	private String getTypeFull() {
 		switch(getType()) {
 		case EMPLOYEE_TYPE_SALARY:
--- test/employee/EmployeeTest.java
+++ test/employee/EmployeeTest.java
@@ -5,6 +5,7 @@ package employee;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,66 +13,34 @@ import org.junit.Test;
  *
  */
 public class EmployeeTest {
-
-	private final double EPSILON = 0.01;
-	private final int EMP_NUMBER_1 = 20101;
-	private final String NAME_1 = "Arnold";
-	private final String DEPARTMENT_1 = "Finance";
-	private final char TYPE_1 = Employee.EMPLOYEE_TYPE_SALARY;
-	private final double PAY_RATE_1 = 78000;
-	private final double MAX_HOURS_1 = 168;
-	private final int EMP_NUMBER_2 = 20112;
-	private final String NAME_2 = "Bernie";
-	private final String DEPARTMENT_2 = "Personnel";
-	private final char TYPE_2 = Employee.EMPLOYEE_TYPE_HOURLY;
-	private final double PAY_RATE_2 = 20;
-	private double MAX_HOURS_2 = 60;
-	private final int EMP_NUMBER_3 = 20123;
-	private final String NAME_3 = "Carole";
-	private final String DEPARTMENT_3 = "Research";
-	private final char TYPE_3 = Employee.EMPLOYEE_TYPE_CONTRACTOR;
-	private final double PAY_RATE_3 = 70;
-	private final double MAX_HOURS_3 = 30;
-
-	/**
-	 * Test helper method to assert Employee instantiation
-	 * 
-	 * @return emp Employee object with initial information for testing
-	 * @author Colin Cui and rafaelalarcon
-	 */
-	private Employee makeEmployee(int a, String b, String c, char d, double e, double f) {
-
-		Employee emp = new Employee();
-
-		emp.setEmpNo(a);
-		emp.setEmpName(b);
-		emp.setDeparment(c);
-		emp.setType(d);
-		emp.setPayRate(e);
-		emp.setMaxHours(f);
-
-		return emp;
+	
+	
+	Employee[] empList;
+	
+	@Before	
+	public void setup() {
+		empList = new Employee[] {
+				new Employee(20101, "Arnold", "Finance", 'S', 78000.0, 168.0),
+				new Employee(20112, "Bernie", "Personnel", 'H', 20.0, 60.0),
+				new Employee(20123, "Carole", "Research", 'C', 70.0, 30.0),
+		};
 	}
 
-	/**
-	 * Test method for
-	 * {@link employee.Employee#Employee(int, java.lang.String, java.lang.String, char, double, double)}.
-	 */
-	@Test
-	public void testEmployeeIntStringStringCharDoubleDouble() {
-		Employee emp_1 = makeEmployee(EMP_NUMBER_1, NAME_1, DEPARTMENT_1, TYPE_1, PAY_RATE_1, MAX_HOURS_1);
-		Employee emp = makeEmployee(20101, "Arnold", "Finance", 'S', 78000, 168);
-		assertEquals("Bad employee's number", emp_1.getEmpNo(), 20101);
-	}
-
+	
 	/**
 	 * Test method for {@link employee.Employee#calcNetPay(double)}.
 	 */
 	@Test
 	public void testCalcNetPay() {
-		Employee emp_2 = makeEmployee(EMP_NUMBER_2, NAME_2, DEPARTMENT_2, TYPE_2, PAY_RATE_2, MAX_HOURS_2);
-		assertEquals("Calculation of net pay is wrong", emp_2.calcNetPay(emp_2.calcGrossPay(61)),
-				(1400 - 48 - 75 - 66.5 - 25.2 - 18.20 - 14), EPSILON);
+		
+		double netPay0 = empList[0].calcNetPay(20);
+		assertEquals (1247.25, netPay0, 0.01);
+		
+		double netPay1 = empList[1].calcNetPay(61);
+		assertEquals ((1400 - 48 - 75 - 66.5 - 25.2 - 18.20 - 14), netPay1, 0.01);
+		
+		double netPay2 = empList[2].calcNetPay(120);
+		assertEquals (1750.45, netPay2, 0.01);
 }
 
 	/**
@@ -79,14 +48,9 @@ public class EmployeeTest {
 	 */
 	@Test
 	public void testCompareTo() {
-
-		// Object with the three types of employees for testing
-		Employee emp_1 = makeEmployee(EMP_NUMBER_1, NAME_1, DEPARTMENT_1, TYPE_1, PAY_RATE_1, MAX_HOURS_1);
-		Employee emp_2 = makeEmployee(EMP_NUMBER_2, NAME_2, DEPARTMENT_2, TYPE_2, PAY_RATE_2, MAX_HOURS_2);
-		Employee emp_3 = makeEmployee(EMP_NUMBER_3, NAME_3, DEPARTMENT_3, TYPE_3, PAY_RATE_3, MAX_HOURS_3);
-
-		assertEquals("Incorrect comparison -1", -1, emp_1.compareTo(emp_3));
-		assertEquals("Employee's number should be the same", 0, emp_1.compareTo(emp_1));
-		assertEquals("Incorrect comparison 1", 1, emp_3.compareTo(emp_1));
+		
+		assertEquals(1, empList[2].compareTo(empList[0]));
+		assertEquals(0, empList[0].compareTo(empList[0]));
+		assertEquals(-1, empList[0].compareTo(empList[2]));
 	}
 }
